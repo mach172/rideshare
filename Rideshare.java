d6a09de3cd749f368134b8573f08edede4832398
Henry Miller <hmbomb1221@gmail.com>
2026-02-25 14:28:02 -0800

    did rideshare file (needs fixing)

--- Rideshare.java
+++ Rideshare.java
@@ -2,34 +2,53 @@ import java.util.*;
 
 /*
 TODO: 
-advance cars
-    check passengers
-    check car
+check system (not working)
 toStrings
 */
 
 public class Rideshare {
     public static void main(String[] args) {
-        Road theRoad = new Road();
+        Road theRoad = new Road(30);
 
-        ArrayList<Passenger> passengerActiveList = new ArrayList<Passenger>();
+        //makes all passengers
+        ArrayList<Passenger> passengerMasterList = new ArrayList<Passenger>();
         for(int i = 0; i < 50; i++){
-            passengerActiveList.add(new Passenger());
-            theRoad.addPassengerStation(i, passengerActiveList.get(i));
+            passengerMasterList.add(new Passenger());
+            theRoad.addPassengerStation(i, passengerMasterList.get(i));
         }
 
-        ArrayList<Car> carActiveList = new ArrayList<Car>();
-        //CHANGE I BOUNDS TO CHANGE NUMBER OF CARS
-        //TODO: WRITE INPUT SYSTEM??
-        for(int i = 1; i <= 30; i++){
-            carActiveList.add(new Car());
+        while(true){
+            boolean allArrived = true;
+            for(Car a : theRoad.getCarList()){
+                if(!a.getArrived()){
+                    allArrived = false;
+                    break;
+                }
+            }
+            if(allArrived){
+                break;
+            }
+            theRoad.advanceAllCars();
         }
-        
-        ArrayList<Car> carCompletedList = new ArrayList<Car>();
 
-        ArrayList<Station> stationMasterList = new ArrayList<Station>();
-        for(int i = 1; i <= 32; i++){
-            stationMasterList.add(new Station(i));
+        int completedCountMasterList = 0;
+        for(Passenger a : passengerMasterList){
+            if(a.getArrivedPassenger()){
+                completedCountMasterList++;
+            }
         }
+
+        System.out.println("Completed Count by master list: " + completedCountMasterList);
+
+        int completedCountStationList = 0;
+        for(Station a : theRoad.getStationList()){
+            for(Passenger b : a.getCompletedPassengerList()){
+                if(b.getArrivedPassenger()){
+                    completedCountStationList++;
+                }
+            }
+        }
+
+        System.out.println("Completed Count by station list: " + completedCountStationList);
     }
 }
